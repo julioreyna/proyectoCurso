cfef06899ee7415a9354782a87d1b925e5047a4d
julioreyna <julioreyna.it@gmail.com>
2023-03-17 13:06:17 -0300

    Creacion de DTO y Script de Base de Datos

---
+++ src/Shared/ProyectoCurso.Shared.DTO/Educacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCurso.Shared.DTO
+{
+    public class Educacion
+    {
+        public int Id { get; set; }
+        public int Userid { get; set; }
+        public DateOnly FechaInicio { get; set; }
+        public DateOnly FechaFin { get; set; }
+        public string NombreCarrera { get; set; }
+        public string Universidad { get; set; }
+        public string Ciudad { get; set; }
+        public string Pais { get; set; }
+
+
+    }
+}
---
+++ src/Shared/ProyectoCurso.Shared.DTO/Intereses.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCurso.Shared.DTO
+{
+    internal class Intrereses
+    {
+    }
+}
